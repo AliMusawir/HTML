21f54d0f0048cd954f454e512d5f6730718a3f76
Musafar Ali Soomro <106115813+AliMusawir@users.noreply.github.com>
2024-02-27 00:09:54 +0500

    Create task2.js

---
+++ week5/task2.js
@@ -0,0 +1,8 @@
+cars=1
+brand: 'Toyota', model: 'Camry', year:
+2022
+function carInfo(carObject) {
+return 'The ${carObject.brand} $
+{carObject.model} was manufactured in $ {carobject.year}. ;
+const newobj=carInfo(cars);
+console. 10g (newobj ;
