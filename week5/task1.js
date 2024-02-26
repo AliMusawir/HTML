4d073d6a3dacc326bf231183452e18b7d8770716
Musafar Ali Soomro <106115813+AliMusawir@users.noreply.github.com>
2024-02-27 00:09:24 +0500

    Create task1.js

---
+++ week5/task1.js
@@ -0,0 +1,9 @@
+function reverseArray (arr){
+let reverseArray=ll;
+for(let i=arr. length-1; 1>=0; 1--){
+reverseArray. push(arr[i]);
+}return reverseArray;
+｝
+let myArr=[3, 4, 5,6,71;
+let returnArr=reverseArray (myArr);
+console. log (returnArr);
