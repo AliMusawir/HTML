89917bc4c2b3027d25492c47f08ce27b163f50bb
Musafar Ali Soomro <106115813+AliMusawir@users.noreply.github.com>
2024-04-01 14:51:20 +0500

    Add files via upload

---
+++ Lab/index.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    name: "Scooter",
+    price: 199.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Scooter",
+  },
+  {
+    name: "Headphones",
+    price: 99.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Headphones",
+  },
+  {
+    name: "Smartphone",
+    price: 599.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Smartphone",
+  },
+  {
+    name: "Laptop",
+    price: 999.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Laptop",
+  },
+  {
+    name: "Watch",
+    price: 149.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Watch",
+  },
+  {
+    name: "Sunglasses",
+    price: 49.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Sunglasses",
+  },
+  {
+    name: "Backpack",
+    price: 79.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Backpack",
+  },
+  {
+    name: "Gaming Console",
+    price: 399.99,
+    imageUrl: "https://api.dicebear.com/8.x/icons/svg?seed=Gaming%20Console",
+  },
+];
+
+const container = document.getElementById("product-container");
+// list in the cart
+const cartlist = document.getElementById("cart_list"); 
+
+products.forEach((product) => {
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("product");
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+
+  const image = document.createElement("img");
+  image.src = product.imageUrl;
+  image.alt = product.name;
+  image.classList.add("product-image");
+
+  imageContainer.appendChild(image);
+
+  const infoDiv = document.createElement("div");
+  infoDiv.classList.add("info");
+
+  const name = document.createElement("h2");
+  name.textContent = product.name;
+
+  const price = document.createElement("p");
+  price.textContent = "$" + product.price.toFixed(2);
+
+  const button = document.createElement("button");
+  button.textContent = "Add to Cart";
+  button.classList.add("add-to-cart-btn");
+
+  // event linter to add products in the cart 
+  button.addEventListener("click", () => {
+    addToCart(product);
+  });
+
+  infoDiv.appendChild(name);
+  infoDiv.appendChild(price);
+  infoDiv.appendChild(button);
+
+  productDiv.appendChild(imageContainer);
+  productDiv.appendChild(infoDiv);
+
+  container.appendChild(productDiv);
+});
+
+ //function to add products in the cart
+function addToCart(product) {
+  const listitem = document.createElement("li");
+  listitem.textContent = product.name + " $" + product.price.toFixed(2);
+  cartlist.appendChild(listitem);
+}
