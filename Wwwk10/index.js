772775ecdcadaeddac740fb8438d08c1c0f1f5cc
Musafar Ali Soomro <106115813+AliMusawir@users.noreply.github.com>
2024-05-23 19:54:59 +0500

    Delete Wwwk10 directory

--- Wwwk10/index.js
+++
@@ -1,9 +0,0 @@
-function sum(...numbers) {
-    return numbers.reduce((total, num) => total + num, 0);
-  }
-  
- 
-  const numbersArray = [1, 2, 3, 4, 5];
-  const result = sum(...numbersArray);
-  console.log(result); 
-  
